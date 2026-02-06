fa9a07e3bb61bc01937ed33ffa1a4edc3ca9dabe
MCiurzynski <matciurzynski@gmail.com>
2026-02-06 20:37:14 +0100

    Removed recorder that didn't record anything and removed function that wasn't used

--- src/detector/main.py
+++ src/detector/main.py
@@ -27,36 +27,6 @@ def get_source():
     elif Config.MODE == 'RTSP': return RTSPSource(Config.get_rtsp_url())
     else: raise ValueError(f"Unknown mode: {Config.MODE}")
 
-def initialize_recorder(frame, recorder):
-    if Config.RECORD and recorder is None:
-        h, w = frame.shape[:2]
-        return cv2.VideoWriter(Config.RECORD_NAME, cv2.VideoWriter_fourcc(*'MJPG'), 30, (w, h))
-    return recorder
-
-def process_batch(batch_frames, models, anonymizer, detector, recorder):
-    
-    pose_results_batch = models["pose"]["model"](batch_frames, verbose=False)
-    plates_results_batch = models["plates"]["model"](batch_frames, verbose=False)
-
-    processed_frames = []
-
-    for frame, pose_results, plates_results in zip(batch_frames, pose_results_batch, plates_results_batch):
-        
-        anonymizer.anonymize(frame, pose_results, "pose")
-        anonymizer.anonymize(frame, plates_results, "box")
-        
-        saved_file = detector.process(frame, pose_results)
-        
-        if saved_file:
-            print(f"[INFO] Zdarzenie zapisane do pliku: {saved_file}")
-
-        if Config.RECORD and recorder:
-            recorder.write(frame)
-            
-        processed_frames.append(frame)
-
-    return processed_frames
-
 def main():
     print(f"MODE: {Config.MODE} | SOURCE: {Config.SOURCE_PATH} | BATCH SIZE: {Config.BATCH_SIZE}")
 
@@ -70,29 +40,19 @@ def main():
     
     detector = Detector(models, anonymizer)
 
-    recorder = None
-    batch_buffer = [] 
+    batch_buffer = []
     fps_estimator = FPS_estimator()
 
     try:
         while True:
             ret, frame = source.get_frame()
             
-            if not ret or frame is None:
-                if len(batch_buffer) > 0:
-                    detector.process_batch_multiperson(batch_buffer)
-                print("End of stream.")
+            if ret:
+                batch_buffer.append(frame)
+            else:
                 break
-
-            if Config.VIDEO_WIDTH and Config.VIDEO_HEIGHT:
-                frame = cv2.resize(frame, (Config.VIDEO_WIDTH, Config.VIDEO_HEIGHT))
-
-            recorder = initialize_recorder(frame, recorder)
-
-            batch_buffer.append(frame)
-
-            if len(batch_buffer) >= Config.BATCH_SIZE:
                 
+            if len(batch_buffer) >= Config.BATCH_SIZE:
                 processed_batch = detector.process_batch_multiperson(batch_buffer)
                 
                 fps_estimator.end()
@@ -100,6 +60,8 @@ def main():
 
                 if Config.SHOW_VIDEO:
                     for p_frame in processed_batch:
+                        if Config.VIDEO_WIDTH and Config.VIDEO_HEIGHT:
+                            p_frame = cv2.resize(p_frame, (Config.VIDEO_WIDTH, Config.VIDEO_HEIGHT))
                         cv2.putText(p_frame, f'FPS: {avg_fps:.2f}', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255), 2)
                         cv2.imshow('Anonymized Video', p_frame)
                         
@@ -114,8 +76,6 @@ def main():
         print("Stopped by user")
     finally:
         source.release()
-        if recorder:
-            recorder.release()
         cv2.destroyAllWindows()
 
 if __name__ == "__main__":
